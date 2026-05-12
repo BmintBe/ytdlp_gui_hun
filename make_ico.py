3c8e27a1dead0de026970ee086b3947d0afa4ca7
BmintBe <148057208+BmintBe@users.noreply.github.com>
2026-05-12 17:25:16 +0200

    Add files via upload

---
+++ make_ico.py
@@ -0,0 +1,3 @@
+from PIL import Image
+img = Image.open('icon.png').resize((256,256), Image.LANCZOS).convert('RGBA')
+img.save('icon.ico', format='ICO', sizes=[(16,16),(32,32),(48,48),(64,64),(128,128),(256,256)])
